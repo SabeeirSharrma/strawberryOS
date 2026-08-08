a7ec949285417c3b3175771ea204f7358510b27a
SabeeirSharrma <sabplayzthepro@gmail.com>
2026-08-08 12:45:08 +0530

    Finish up auto start service and strawberry-cli/strawberryd burn-in

--- archiso/profiledef.sh
+++ archiso/profiledef.sh
@@ -19,4 +19,5 @@ airootfs_image_tool_options=('-comp' 'zstd' '-Xcompression-level' '19')
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
   ["/usr/local/bin/strawberry-cli"]="0:0:755"
+  ["/usr/local/bin/strawberryd"]="0:0:755"
 )
